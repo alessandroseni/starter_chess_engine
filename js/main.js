a34b7899e5fc255ffc2c21cc3ca78601e62f6dd8
Azeni <alessandroseni@gmail.com>
2023-07-27 16:06:54 -0500

    chessing the engine

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-var board = null;
-var game = new Chess();
+const game = new Chess();
+let board = null;
 
 function makeRandomMove() {
   // chess.js gives us all the possible moves in an array
   // [ move1, move2, move3 ... ]
-  var possibleMoves = game.moves();
+  const possibleMoves = game.moves();
 
   // exit if the game is over
   if (game.game_over()) return;
 
-  // choses a random index in the list
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  // chooses a random index in the list
+  const randomIdx = Math.floor(Math.random() * possibleMoves.length);
 
   // updates javascript board state
   game.move(possibleMoves[randomIdx]);
@@ -19,9 +19,14 @@ function makeRandomMove() {
   board.position(game.fen());
 
   // call this function again in 5 secs
-  window.setTimeout(makeRandomMove, 500);
+  setTimeout(makeRandomMove, 500);
+}
+
+function printPGN() {
+  const pgn = game.pgn();
+  console.log(pgn);
 }
 
 board = Chessboard("myBoard", "start");
-
-window.setTimeout(makeRandomMove, 500);
+setTimeout(makeRandomMove, 500);
+setInterval(printPGN, 10000); // Print PGN every 10 seconds
